8901289f64ab6757629c4339b5b39907eb52b611
Tariqul Islam <youandtariq@gmail.com>
2021-03-17 10:07:38 +0600

    debugging, configuring env and many more

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 //Modules
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const config = require('config');
 const express = require('express');
 //Routers Path
 const home = require('./routes/home');
@@ -8,9 +13,28 @@ const log = require('./middleWares/logger')
 
 //The Express App
 const app = express();
+
+//Setting up environment variables
+// console.log(`node env: ${process.env.NODE_ENV}`);
+// console.log(`app: ${app.get('env')}`);
+
+//Setting configuration
+console.log(config.get('name'));
+console.log(config.get('host.password'));
+
 //Middlewares
 app.use(express.json());
-app.use(log);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(helmet());
+//Logging
+if (app.get('env') === 'development') {
+  app.use(log);
+  app.use(morgan('tiny'));
+  startupDebugger('StartUp debugging....');
+  dbDebugger('Database debugging....');
+}
+
 //Routers
 app.use('/', home);
 app.use('/api/genres', genre);
